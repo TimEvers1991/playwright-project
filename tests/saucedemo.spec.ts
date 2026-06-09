e35b4e4ea1c9a9ae74d142f57ac31a9ce844bea0
TimEvers1991 <TimEvers1991@://github.com>
2026-06-09 17:05:17 +0200

    eerste branch aangemaakt en nu proberen te committen

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -150,11 +150,13 @@ test.describe("Saucedemo logging in & shopping flow", () => {
     await expect(page).toHaveURL(/checkout-step-two\.html$/);
     await expect(page.locator(".cart_item")).toHaveCount(1);
 
-    // Finish - complete order
-    await page.getByRole("button", { name: "Finish" }).click();
+    // Finish: complete orders
+    await page.getByRole("button", { name: "Finish" }).click(); //bezig met branch test, kan je later verwijderen
     await expect(page).toHaveURL(/checkout-complete\.html$/);
-    await expect(page.getByText("Thank you for your order")).toBeVisible();
+    // await expect(page.getByText("Thank you for your order")).toBeVisible();
   });
+
+  console.log("Hoi, ben bezig met een branch test");
 });
 
 // ............
